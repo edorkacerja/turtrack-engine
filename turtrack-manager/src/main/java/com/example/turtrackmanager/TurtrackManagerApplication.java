ec2bc237480b741385a712b6b1a6b6e93504b24e
Intellicode LLC <intellicodellc27@gmail.com>
2024-08-21 23:14:49 -0600

    added unversioned files

---
+++ turtrack-manager/src/main/java/com/example/turtrackmanager/TurtrackManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.turtrackmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtrackManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtrackManagerApplication.class, args);
+	}
+
+}
